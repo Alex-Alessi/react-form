8563815a73607642a065b52318fbcc74d34c5c4e
Alex-Alessi <alex.alessi246@gmail.com>
2024-12-05 16:00:55 +0100

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
-// import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
+  const titleField = "";
+  const articleList = [];
+
+  const handleInsertPostSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <div className="container">
-        <h1 className="mt-5">Hello World</h1>
+        <section className="py-4">
+          <form onSubmit={handleInsertPostSubmit}>
+            <h4>Insert form</h4>
+            <div className="row">
+              <div className="col-4">
+                <label className="form-label" htmlFor="post-title">
+                  Titolo
+                </label>
+                <input type="text" className="form-control" id="post-title" />
+              </div>
+
+              <div className="col-12">
+                <button className="btn btn-success my-2">Crea post</button>
+              </div>
+            </div>
+          </form>
+        </section>
+        <section>
+          <h4>Post list</h4>
+          <div className="row">
+            <div className="col-4">
+              <div className="card">
+                <div className="card-body">
+                  <h2>Post title</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
